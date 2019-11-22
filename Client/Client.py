f6c080e132229f2cf37f984cec63b91b0e69b715
drakamon <sebastiantords@gmail.com>
2019-11-22 02:40:26 +0100

    bound port and ip

--- Client/Client.py
+++ Client/Client.py
@@ -1,2 +1,22 @@
 import socket
 import cv2
+
+
+
+Port=1234
+IP="127.0.0.1"
+
+# create the socket
+client.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# set socket options
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+# bind port and ip to socket
+server_socket.bind((IP, Port))
+
+
+
+
+
+
