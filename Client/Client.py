3fe665e111c65d9efbde231f4755b3fa7b36fbcd
drakamon <sebastiantords@gmail.com>
2019-11-22 02:48:27 +0100

    added opencv and listen

--- Client/Client.py
+++ Client/Client.py
@@ -1,4 +1,5 @@
 import socket
+import select
 import cv2
 
 
@@ -7,7 +8,7 @@ Port=1234
 IP="127.0.0.1"
 
 # create the socket
-client.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # set socket options
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -15,8 +16,12 @@ server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 # bind port and ip to socket
 server_socket.bind((IP, Port))
 
+# turn on listen
+server_socket.listen()
 
+# list sockets
+sockets_list = [server_socket]
 
-
-
+# list clinets
+clients = {}
 
