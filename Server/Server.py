502c96b90c114693fb260e13b8ab72a43436e02d
drakamon <sebastiantords@gmail.com>
2019-11-22 04:15:44 +0100

    added numpy and text box

--- Server/Server.py
+++ Server/Server.py
@@ -1,16 +1,17 @@
 import socket
 import select
 import cv2
+import numpy as np
 
 Port=1234
 IP="127.0.0.1"
 HeadLength=10
 
-serverSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-serverSocket.bind((IP, Port))
-serverSocket.listen()
-socketsList = [serverSocket]
+S=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+S.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+S.bind((IP, Port))
+S.listen()
+socketsList = [S]
 Clients = {}
 print("Started server")
 def getMessage(clientSocket):
@@ -25,10 +26,14 @@ def getMessage(clientSocket):
 while True:
     readSockets,_,exceptionsSockets=select.select(socketsList,[],socketsList)
     for notifySockets in readSockets:
-        if notifySockets==serverSocket:
-            clientSocket,clientAddress=serverSocket.accept()
+        if notifySockets==S:
+            clientSocket,clientAddress=S.accept()
             socketsList.append(clientSocket)
             print("New connection")
         else:
             Message=getMessage(notifySockets)
             print("Recieved message: " + Message["data"].decode("utf-8"))
+            image = np.zeros((512, 512, 3))
+            cv2.putText(image, Message["data"].decode("utf-8"), (200, 200), 0, 1, 255)
+            cv2.imshow('Image', image)
+            cv2.waitKey(1)
